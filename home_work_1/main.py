bf656b5cde16d48177932c03c3cd1bcd0cb5cb16
Dmitqa <koraduba57@gmail.com>
2025-03-13 21:07:25 +0300

    Update main.py

--- home_work_1/main.py
+++ home_work_1/main.py
@@ -1,15 +1,7 @@
-"""
-Домашнее задание №1
-Функции и структуры данных
-"""
-
-
 def power_numbers(*numbers: int) -> list:
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
-    >>> power_numbers(1, 2, 5, 7)
-    <<< [1, 4, 25, 49]
     """
     return list(map(lambda num: num ** 2, numbers))
 
@@ -42,11 +34,6 @@ def filter_numbers(num_list: list[int], filter_type: str) -> list[int] | None:
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
     (выбор производится передачей дополнительного аргумента)
-
-    >>> filter_numbers([1, 2, 3], ODD)
-    <<< [1, 3]
-    >>> filter_numbers([2, 3, 4, 5], EVEN)
-    <<< [2, 4]
     """
     if filter_type == ODD:
         return list(filter(lambda el: el % 2, num_list))
